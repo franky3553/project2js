85d832f33ef75575ee0312410aa83f89f70264b5
franky3553 <franky3553@gmail.com>
2022-10-09 11:01:37 +0200

    Add makePcDecisionFunction
    Adding random number to a pc decision
    Depending on the number, one option or another will come out

--- app.js
+++ app.js
@@ -4,9 +4,31 @@ const outputAppears = document.getElementById('output')
 const allDecisions = document.querySelectorAll('button')
 
 let userDecision
+let pcDecision
 
 allDecisions.forEach(decision => decision.addEventListener('click', (e) => {
 	userDecision = e.target.id
 	userDecisionAppears.innerHTML = userDecision
 
+   makePcDecision()
+
 }))
+
+
+function makePcDecision() {
+	const randomNumber = Math.floor(Math.random() * 3) + 1	
+	
+	if (randomNumber === 0) {
+		pcDecision = 'rock'
+	}
+
+	if (randomNumber === 1) {
+		pcDecision = 'paper'
+	}
+
+	if (randomNumber === 2) {
+		pcDecision = 'scissors'
+	}
+	
+	pcDecisionAppears.innerHTML = pcDecision
+}

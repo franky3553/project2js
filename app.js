efeb88859609e34799ada10694ec3a688baef1fd
franky3553 <franky3553@gmail.com>
2022-10-09 11:00:00 +0200

    Add allDecisions clickEvent
    First, userDecision clickEvent

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 const pcDecisionAppears = document.getElementById('pc-decision')
 const userDecisionAppears = document.getElementById('user-decision')
 const outputAppears = document.getElementById('output')
+const allDecisions = document.querySelectorAll('button')
+
+let userDecision
+
+allDecisions.forEach(decision => decision.addEventListener('click', (e) => {
+	userDecision = e.target.id
+	userDecisionAppears.innerHTML = userDecision
+
+}))
